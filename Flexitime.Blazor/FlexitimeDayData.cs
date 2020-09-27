71335a4d980fabde2af57637f50af7a8da24f243
Patrick McMichael <patrick@patrickmcmichael.org>
2020-09-27 18:47:20 +0100

    Fix DateTime vs TimeSpan

--- Flexitime.Blazor/FlexitimeDayData.cs
+++ Flexitime.Blazor/FlexitimeDayData.cs
@@ -16,15 +16,19 @@ namespace Flexitime.Blazor
 
         public List<string> Validate()
         {
-            WorkedTimeDay = EndTime - StartTime - BreakTime.TimeOfDay;
+            TimeSpan startTimeSpan = new TimeSpan(0, StartTime.Hour, StartTime.Minute, 0);
+            TimeSpan endTimeSpan = new TimeSpan(0, EndTime.Hour, EndTime.Minute, 0);
+            TimeSpan breakTimeSpan = new TimeSpan(0, BreakTime.Hour, BreakTime.Minute, 0);
+
+            WorkedTimeDay = endTimeSpan - startTimeSpan - breakTimeSpan;
             List<string> errors = new List<string>();
             if (StartTime > EndTime)
             {
                 errors.Add("End time cannot be before Start time");
             }
 
-            TimeSpan workedTime = EndTime - StartTime;
-            if (workedTime.TotalSeconds < BreakTime.TimeOfDay.TotalSeconds)
+            TimeSpan workedTime = endTimeSpan - startTimeSpan;
+            if (workedTime.TotalSeconds < breakTimeSpan.TotalSeconds)
             {
                 errors.Add("Break time cannot be longer than total work time");
             }
